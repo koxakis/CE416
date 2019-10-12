bd54793889b67312c9a11dbc74f9e71656dc1fb6
Nikolas Koxenoglou <koxakin@gmail.com>
2019-10-12 23:22:32 +0300

    Added comments and builded a basic shader

--- Tutorial/tut_video/Application.cpp
+++ Tutorial/tut_video/Application.cpp
@@ -2,8 +2,6 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-/* BUILD a function for legacy triangle */
-
 /* position data for the buffer */
 float positions[6] =
 {
@@ -14,14 +12,14 @@ float positions[6] =
 
 void legacy_triangles()
 {
-	/* Begin legacy openGL triagle routine */
+	/* Begin legacy openGL triangle routine */
 	/* This can be used without the Glew Init */
 	glBegin(GL_TRIANGLES);
 
-	/* Specify 3 veritcies for triagles in 2D */
-	/* By defult projection matrix in openGL is between -1.0 and 1.0 in aevery dimention in every axis */
+	/* Specify 3 vertices for triangles in 2D */
+	/* By defult projection matrix in openGL is between -1.0 and 1.0 in every dimensions in every axis */
 
-	/* Change the colour to yello */
+	/* Change the colour to yellow */
 	glColor3f(1, 1, 0);
 
 	/* Upper triangle */
@@ -43,63 +41,95 @@ void legacy_triangles()
 	glEnd();
 }
 
-/* complile the shader from the given source */
+/* Compile the shader from the given source */
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
-	unsigned int id = glCreateShader(GL_VERTEX_SHADER);
+	/* Specifies the type of shader to be created of type */
+	unsigned int id = glCreateShader(type);
 
+	/* This returns pointer to the begining of the data */
+	/* Source needs to exists, if source goes out of scope 
+			this will return garbage												*/
 	const char* src = source.c_str();
 
+	/* Point to source character data */
 	glShaderSource(id, 1, &src, nullptr);
 
+	/* Compile the shader given by the id */
 	glCompileShader(id);
 
 	int result;
+	/* Return the result */
 	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
 
+	/* Error handling */
 	if (result == GL_FALSE)
-	{
-		int length;
+		{
+			int length;
 
-		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+			/* Returns a parameter from a shader object in order to identify the problem shader */
+			glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
 
-		char* message = (char*)alloca(length* sizeof(char));
+			/* Allocate memory in Stack for error message ( not advisable ) */
+			char* message = (char*)alloca(length* sizeof(char));
 
-		glGetShaderInfoLog(id, length, &length, message);
+			/* Specifies the shader object whose information log is to be queried */
+			glGetShaderInfoLog(id, length, &length, message);
 
-		std::cout << "Faild to complile" <<
-			(type == GL_VERTEX_SHADER ? "vertex" : "fragment") << "shader" << std::endl;
+			/* Print the shader failed massage with the accompanying message */
+			std::cout << "Failed to compile" <<
+				(type == type ? "vertex" : "fragment") << "shader" << std::endl;
 
-		std::cout << message << std::endl;
+			std::cout << message << std::endl;
 
-		glDeleteShader(id);
+			/* Delete the problem shader */
+			glDeleteShader(id);
 
-		return 0;
+			return 0;
 
-	}
+		}
 
 	return id;
-
 }
 
-/* Create and link the shader ( both vertext and fragment */
-static unsigned int Create_Shader(const std::string& vertexShader, const std::string& fragmentShader)
+/* Create, compile and link the shader ( both vertext and fragment )*/
+/* These strings are mend to be the actual source code of the shaders */
+/* Can be read from files or exists as strings or genereded on the fly */  
+/* Return a unique identifier for that program as per buffer in order to bind it */
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
 {
 	/* Create a program and return an identifier to it */
 	unsigned int program = glCreateProgram();
 
 	/* create 2 shader objects */
+	/* Vertex Shader is a part of the graphics pipeline */
+	/* When we issue a draw call the Vertext shader gets
+	 called first and then the Fragment shader 			*/
+	/* In the triangle we have 3 verticies -> this will be called 3 times 
+		to tell where in the window you want the vertex to be */ 
 	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
 
+	/* Fragment Shader is a part of the graphics pipeline */
+	/* In simple terms there are many stages in the pipeline
+		before, after and between the shaders and the rasterization */
+	/* Begining of draw call-> ... -> Shader(Vertex) -> ... Shader(Fragment) -> ... 
+		-> rasterization -> ... -> pixels on screen */
+	/* fragments aren't exactly pixels, the fragmed shader will run one time for each
+		pixel that is going to be rasterized(Fill the triangle with pixels) to deside 
+		witch colour(or other attribute) the pixel will be */
 	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
 
+	/* After the compilation we need to attach these shaders to our program */
 	glAttachShader(program, vs);
 	glAttachShader(program, fs);
 
+	/* Link the program */
 	glLinkProgram(program);
 
+	/* Validate program */
 	glValidateProgram(program);
 
+	/* Delete unneeded shaders due to the linking to the program we can delete the intermediate obj files */
 	glDeleteShader(vs);
 	glDeleteShader(fs);
 
@@ -115,7 +145,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
 	if (!window)
 		{
 			glfwTerminate();
@@ -133,11 +163,11 @@ int main(void)
 	/* After this point we can use all the OpenGL functionality */
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	/* Define the data outside the loop, updades can happen in loop*/
+	/* Define the data outside the loop, updates can happen in loop */
 	/* Define OpenGL buffers */
 	
 	unsigned int buffer;
-	/* This creates a buffer and returns the ID in that form*/
+	/* This creates a buffer and returns the ID in that form */
 	glGenBuffers(1, &buffer);
 
 	/* Select == bind this buffer to use it */
@@ -146,24 +176,42 @@ int main(void)
 	glBindBuffer( GL_ARRAY_BUFFER, buffer);
 
 	/* Provide the buffer with data */
-	/* You can provide the data later if need be*/
+	/* You can provide the data later if need be */
 	glBufferData( GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
-	/* Define a verctex atribiute */
+	/* Define a vertex attribute */
 	glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
-	/* Enable of the generic vertex atribioute */
+	/* Enable of the generic vertex attribute */
 	glEnableVertexAttribArray(0);
 
-	// stopped at writing a shader in openGL 24:32
+	/* String code for vertex shader */
 	std::string vertexShader =
-		"#version 330 core\n"
+		"#version 460 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
 		"\n"
 		"void main()\n"
 		"{\n"
-		""
+		"	gl_Position = position;\n"
 		"}\n";
-	//unsigned int shader = Create_Shader();
+
+	/* String code for vertex shader */
+	std::string fragmentShader =
+		"#version 460 core\n"
+		"\n"
+		"layout(location = 0) out vec4 colour;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	colour = vec4(1.0, 1.0, 0.0, 1.0);\n"
+		"}\n";
+	
+	/* Create shader using the above code */
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+
+	/* Bind shader */
+	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -187,6 +235,8 @@ int main(void)
 			/* Poll for and process events */
 			glfwPollEvents();
 		}
+
+	glDeleteProgram(shader);
 
 	glfwTerminate();
 	return 0;
