3b0358cdcbc47aa3c1b630dfd2b5f7bffffe4449
Nikolas Koxenoglou <koxakin@gmail.com>
2019-10-28 11:43:41 +0200

    Added further comments and indentation to tutorial code

--- Tutorial/tut_site/simple_trasform/app.cpp
+++ Tutorial/tut_site/simple_trasform/app.cpp
@@ -22,20 +22,21 @@ int main( void )
 {
 	/* Initialize the library */
 	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
+		{
+			fprintf( stderr, "Failed to initialize GLFW\n" );
+			getchar();
+			return -1;
+		}
 
-	/* Adding extra openGL options */
+	/* Adding extra openGL options and version */
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL 
+	/* We don't want the old OpenGL */
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  
 
-	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Tutorial 03 - Matrices", NULL, NULL);
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow( 1024, 768, "Hello World", NULL, NULL);
 	if( window == NULL )
 		{
 			fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
@@ -43,105 +44,115 @@ int main( void )
 			glfwTerminate();
 			return -1;
 		}
+	
+	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	// Initialize GLEW
+	/* Move glew init as per doc after we have a valid openGL context */
+	/* Initialize glew */
 	glewExperimental = true; // Needed for core profile
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
+	if (glewInit() != GLEW_OK) 
+		{
+			fprintf(stderr, "Failed to initialize GLEW\n");
+			getchar();
+			glfwTerminate();
+			return -1;
+		}
 	/* After this point we can use all the OpenGL functionality */
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	// Ensure we can capture the escape key being pressed below
+	/* Ensure we can capture the escape key being pressed below */
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
+	/* Dark blue background */
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
+	/* Set up the Vertex arrays */
 	GLuint VertexArrayID;
 	glGenVertexArrays(1, &VertexArrayID);
 	glBindVertexArray(VertexArrayID);
 
-	// Create and compile our GLSL program from the shaders
+	/* Create and compile our GLSL program from the shaders */
 	GLuint programID = LoadShaders( "SimpleTransform.vertexshader", "SingleColor.fragmentshader" );
 
-	// Get a handle for our "MVP" uniform
+	/* Get a handle for our "MVP" uniform */
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
 
-	// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+	/* Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units */
 	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-	// Or, for an ortho camera :
+	/* Or, for an ortho camera : */
 	//glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
 	
-	// Camera matrix
+	/* Camera matrix */
 	glm::mat4 View       = glm::lookAt(
 								glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
 								glm::vec3(0,0,0), // and looks at the origin
 								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
 						   );
-	// Model matrix : an identity matrix (model will be at the origin)
+	/* Model matrix : an identity matrix (model will be at the origin) */
 	glm::mat4 Model      = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+	/* Our ModelViewProjection : multiplication of our 3 matrices */
+	/* Remember, matrix multiplication is the other way around */
+	glm::mat4 MVP        = Projection * View * Model; 
 
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f, -1.0f, 0.0f,
-		 1.0f, -1.0f, 0.0f,
-		 0.0f,  1.0f, 0.0f,
-	};
+	static const GLfloat g_vertex_buffer_data[] = 
+		{ 
+			-1.0f, -1.0f, 0.0f,
+			1.0f, -1.0f, 0.0f,
+			0.0f,  1.0f, 0.0f,
+		};
 
 	GLuint vertexbuffer;
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	do{
+	do
+		{
 
-		// Clear the screen
-		glClear( GL_COLOR_BUFFER_BIT );
+			/* Render here */
+			/* Clear the screen */
+			glClear( GL_COLOR_BUFFER_BIT );
+			
+			/* A Shader is a program that runs on the GPU gets the data from buffer ( GPU VRAM ) */
+			/* Use our shader */
+			glUseProgram(programID);
 
-		// Use our shader
-		glUseProgram(programID);
+			/* Send our transformation to the currently bound shader, 
+			/* in the "MVP" uniform */
+			glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-		// Send our transformation to the currently bound shader, 
-		// in the "MVP" uniform
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+			/* 1rst attribute buffer : vertices */
+			glEnableVertexAttribArray(0);
+			glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+			glVertexAttribPointer(
+				0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+				3,                  // size
+				GL_FLOAT,           // type
+				GL_FALSE,           // normalized?
+				0,                  // stride
+				(void*)0            // array buffer offset
+			);
 
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-		);
+			/* Draw the triangle ! */
+			glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
 
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+			glDisableVertexAttribArray(0);
 
-		glDisableVertexAttribArray(0);
+			/* Swap buffers */
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-
-	} // Check if the ESC key was pressed or the window was closed
+		} /* Check if the ESC key was pressed or the window was closed */
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
 
-	// Cleanup VBO and shader
+	/* Cleanup VBO and shader */
 	glDeleteBuffers(1, &vertexbuffer);
 	glDeleteProgram(programID);
 	glDeleteVertexArrays(1, &VertexArrayID);
 
-	// Close OpenGL window and terminate GLFW
+	/* Close OpenGL window and terminate GLFW */
 	glfwTerminate();
 
 	return 0;
