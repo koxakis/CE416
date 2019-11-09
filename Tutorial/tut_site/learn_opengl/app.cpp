d854d9baea3d58f9baf3fccd180271ad54f33106
Nikolas Koxenoglou <koxakin@gmail.com>
2019-11-09 12:47:07 +0200

    Finished tutorial 5

--- Tutorial/tut_site/learn_opengl/app.cpp
+++ Tutorial/tut_site/learn_opengl/app.cpp
@@ -57,6 +57,8 @@ int main()
 			std::cout << "Failed to initialize GLAD" << std::endl;
 			return -1;
 		}
+	/* After this point we can use all the OpenGL functionality */
+	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	/* Build and compile our shader program */
 	/* vertex shader */
@@ -110,23 +112,36 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	/* Set up vertex data (and buffer(s)) and configure vertex attributes */
-	float vertices[] = 
-		{
-			-0.5f, -0.5f, 0.0f, // left  
-				0.5f, -0.5f, 0.0f, // right 
-				0.0f,  0.5f, 0.0f  // top   
-		}; 
+    float vertices[] = 
+			{
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    	};
+    unsigned int indices[] = 
+			{  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    	};
 
-
-	unsigned int VBO, VAO;
+	/* Vertext Buffer Object, Vertex Array Object, Element Buffer Objects */
+	/* VAO points attributes to positons in the VBO according to the stride as well as an EBO */
+	/* EBO is a buffer, just like a vertex buffer object, that stores indices that OpenGL uses to decide what vertices to draw */
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	/* bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).*/
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	/* we have to specify how OpenGL should interpret the vertex data before rendering */
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
@@ -157,16 +172,22 @@ int main()
 			/* activate by calling glUseProgram */
    		/* Draw our first triangle */
       glUseProgram(shaderProgram);
-
       /* seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized */
-			glBindVertexArray(VAO); 
-    	glDrawArrays(GL_TRIANGLES, 0, 3);			
+      glBindVertexArray(VAO); 
+
+      //glDrawArrays(GL_TRIANGLES, 0, 6);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+      //glBindVertexArray(0); // no need to unbind it every time 
 
 			/* glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.) */
 			glfwSwapBuffers(window);
 			glfwPollEvents();
 		}
 
+	/* optional: de-allocate all resources once they've outlived their purpose: */
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	/* glfw: terminate, clearing all previously allocated GLFW resources. */
 	glfwTerminate();
 
