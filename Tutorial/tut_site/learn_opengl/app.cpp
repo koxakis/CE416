e36d72ed16aacee2ec42b1401940e22b4a9430fb
Nikolas Koxenoglou <koxakin@gmail.com>
2019-11-13 20:26:21 +0200

    Added loading of a simple texure

--- Tutorial/tut_site/learn_opengl/app.cpp
+++ Tutorial/tut_site/learn_opengl/app.cpp
@@ -1,5 +1,6 @@
 #include "app_functions.hpp"
-
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 /* Window settings */
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -58,32 +59,46 @@ int main()
 	/* Set up vertex data (and buffer(s)) and configure vertex attributes */
 	float vertices[] = 
 		{
-			// positions         // colors
-				0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-			-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-				0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+			// positions          // colors           // texture coords
+				0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+				0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+			-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+			-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+		};
+	unsigned int indices[] = 
+		{  
+			0, 1, 3, // first triangle
+			1, 2, 3  // second triangle
 		};
 
 	/* Vertext Buffer Object, Vertex Array Object, Element Buffer Objects */
 	/* VAO points attributes to positons in the VBO according to the stride as well as an EBO */
 	/* EBO is a buffer, just like a vertex buffer object, that stores indices that OpenGL uses to decide what vertices to draw */
-	unsigned int VBO, VAO;
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	/* bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).*/
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	/* we have to specify how OpenGL should interpret the vertex data before rendering */
 	/* position attribute */
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
   /* color attribute */
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
+
+	/* texture coord attribute */
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	/* note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind */
 	glBindBuffer(GL_ARRAY_BUFFER, 0); 
@@ -91,6 +106,33 @@ int main()
 	/* You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
 			VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary. */
 	glBindVertexArray(0); 
+
+	/* load and create a texture */
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture); 
+	/* all upcoming GL_TEXTURE_2D operations now have effect on this texture object */
+	/* set the texture wrapping parameters */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+	/* set texture wrapping to GL_REPEAT (default wrapping method) */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	/* set texture filtering parameters */
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	/* load image, create texture and generate mipmaps */
+	int width, height, nrChannels;
+	/* The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path. */
+	unsigned char *data = stbi_load(FileSystem::getPath("container.jpg").c_str(), &width, &height, &nrChannels, 0);
+	if (data)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+	else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+		}
+	stbi_image_free(data);
 
 	/* Uncomment this call to draw in wireframe polygons. */
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -108,12 +150,15 @@ int main()
 			/* Clear the screens colour buffer */
       glClear(GL_COLOR_BUFFER_BIT);
 
+			/* bind Texture */
+			glBindTexture(GL_TEXTURE_2D, texture);
+
 			/* activate by calling glUseProgram */
    		/* Draw our first triangle */
       ourShader.use();
       /* seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized */
       glBindVertexArray(VAO);
-      glDrawArrays(GL_TRIANGLES, 0, 3);
+      glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 			/* glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.) */
 			glfwSwapBuffers(window);
@@ -123,6 +168,7 @@ int main()
 	/* optional: de-allocate all resources once they've outlived their purpose: */
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	/* glfw: terminate, clearing all previously allocated GLFW resources. */
 	glfwTerminate();
 
--- Tutorial/tut_site/learn_opengl/app_functions.hpp
+++ Tutorial/tut_site/learn_opengl/app_functions.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cmath>
 #include "shader_s.h"
+#include "filesystem.h"
 
 void processInput(GLFWwindow *window);
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
