5efe9e1586db6e588b4a18f1a379d32de96be262
Nikolas Koxenoglou <koxakin@gmail.com>
2019-12-04 17:03:07 +0200

    Completed Basic lighting tutorial ( Phong model ) and added to backup folder

---
+++ Tutorial/tut_site/learn_opengl/Basic_lighting_phong_model/app_functions.cpp
@@ -0,0 +1,9 @@
+#include "app_functions.hpp"
+
+/* glfw: whenever the window size changed (by OS or user resize) this callback function executes */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	/* make sure the viewport matches the new window dimensions; note that width and 
+    	height will be significantly larger than specified on retina displays. */
+	glViewport(0, 0, width, height);
+}
