185a0f7193fb5cb4573e45c1e77d3b5879911df3
Nikolas Koxenoglou <koxakin@gmail.com>
2019-12-10 16:58:32 +0200

    Added final lighting stage with multiple light sources

--- Tutorial/tut_site/learn_opengl/Spotlight_realistic/app.cpp
+++ Tutorial/tut_site/learn_opengl/Spotlight_realistic/app.cpp
@@ -167,15 +167,6 @@ int main()
 			glm::vec3( 1.5f,  0.2f, -1.5f),
 			glm::vec3(-1.3f,  1.0f, -1.5f)
   	};
-
-	/* positions of the point lights */
-	glm::vec3 pointLightPositions[] = 
-		{
-			glm::vec3( 0.7f,  0.2f,  2.0f),
-			glm::vec3( 2.3f, -3.3f, -4.0f),
-			glm::vec3(-4.0f,  2.0f, -12.0f),
-			glm::vec3( 0.0f,  0.0f, -3.0f)
-		};
 	/* Vertext Buffer Object, Vertex Array Object, Element Buffer Objects */
 	/* VAO points attributes to positons in the VBO according to the stride as well as an EBO */
 	/* EBO is a buffer, just like a vertex buffer object, that stores indices that OpenGL uses to decide what vertices to draw */
@@ -244,64 +235,21 @@ int main()
 
 			/* be sure to activate shader when setting uniforms/drawing objects */
 			lightingShader.use();
+			lightingShader.setVec3("light.position", camera.Position);
+			lightingShader.setVec3("light.direction", camera.Front);
+			lightingShader.setFloat("light.cutOff", glm::cos(glm::radians(12.5f)));
+      lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.5f)));
 			lightingShader.setVec3("viewPos", camera.Position);
-			lightingShader.setFloat("material.shininess", 32.0f);
 
 			/* light properties */
-			/*
-					Here we set all the uniforms for the 5/6 types of lights we have. We have to set them manually and index 
-					the proper PointLight struct in the array to set each uniform variable. This can be done more code-friendly
-					by defining light types as classes and set their values in there, or by using a more efficient uniform approach
-					by using 'Uniform buffer objects', but that is something we'll discuss in the 'Advanced GLSL' tutorial.
-			*/
-			/* directional light */
-			lightingShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-			lightingShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-			lightingShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
-			lightingShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
-			/* point light 1 */
-			lightingShader.setVec3("pointLights[0].position", pointLightPositions[0]);
-			lightingShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
-			lightingShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
-			lightingShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
-			lightingShader.setFloat("pointLights[0].constant", 1.0f);
-			lightingShader.setFloat("pointLights[0].linear", 0.09);
-			lightingShader.setFloat("pointLights[0].quadratic", 0.032);
-			/* point light 2 */
-			lightingShader.setVec3("pointLights[1].position", pointLightPositions[1]);
-			lightingShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
-			lightingShader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
-			lightingShader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
-			lightingShader.setFloat("pointLights[1].constant", 1.0f);
-			lightingShader.setFloat("pointLights[1].linear", 0.09);
-			lightingShader.setFloat("pointLights[1].quadratic", 0.032);
-			/* point light 3 */
-			lightingShader.setVec3("pointLights[2].position", pointLightPositions[2]);
-			lightingShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
-			lightingShader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
-			lightingShader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
-			lightingShader.setFloat("pointLights[2].constant", 1.0f);
-			lightingShader.setFloat("pointLights[2].linear", 0.09);
-			lightingShader.setFloat("pointLights[2].quadratic", 0.032);
-			/* point light 4 */
-			lightingShader.setVec3("pointLights[3].position", pointLightPositions[3]);
-			lightingShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
-			lightingShader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
-			lightingShader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
-			lightingShader.setFloat("pointLights[3].constant", 1.0f);
-			lightingShader.setFloat("pointLights[3].linear", 0.09);
-			lightingShader.setFloat("pointLights[3].quadratic", 0.032);
-			/* spotLight */
-			lightingShader.setVec3("spotLight.position", camera.Position);
-			lightingShader.setVec3("spotLight.direction", camera.Front);
-			lightingShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-			lightingShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
-			lightingShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
-			lightingShader.setFloat("spotLight.constant", 1.0f);
-			lightingShader.setFloat("spotLight.linear", 0.09);
-			lightingShader.setFloat("spotLight.quadratic", 0.032);
-			lightingShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
-			lightingShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));   
+			lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+			/* we configure the diffuse intensity slightly higher; the right lighting conditions differ with each lighting method and environment.
+					each environment and lighting type requires some tweaking to get the best out of your environment. */
+			lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
+			lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+			lightingShader.setFloat("light.constant", 1.0f);
+			lightingShader.setFloat("light.linear", 0.09f);
+			lightingShader.setFloat("light.quadratic", 0.032f);
 
 			/* material properties */
     	lightingShader.setFloat("material.shininess", 32.0f);
@@ -330,33 +278,31 @@ int main()
 					/* calculate the model matrix for each object and pass it to shader before drawing */
 					glm::mat4 model = glm::mat4(1.0f);
 					model = glm::translate(model, cubePositions[i]);
-					float angle = (20.0f * i) * glfwGetTime();
+					float angle = 20.0f * i;
 					model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 					lightingShader.setMat4("model", model);
 
 					glDrawArrays(GL_TRIANGLES, 0, 36);
 				}
 
-			/* also draw the lamp objects */
+/* 			/* also draw the lamp object 
 			lampShader.use();
 			lampShader.setMat4("projection", projection);
 			lampShader.setMat4("view", view);
 
-			/* many smaller cubes */
+			model = glm::mat4(1.0f);
+			model = glm::translate(model, lightPos);
+			/* a smaller cube 
+			model = glm::scale(model, glm::vec3(0.2f)); 
+			lampShader.setMat4("model", model);
+
 			glBindVertexArray(lightVAO);
-			for (unsigned int i = 0; i < 4; i++)
-				{
-					model = glm::mat4(1.0f);
-					model = glm::translate(model, pointLightPositions[i]);
-					/* Make it a smaller cube */
-					model = glm::scale(model, glm::vec3(0.2f)); 
-					lampShader.setMat4("model", model);
-					glDrawArrays(GL_TRIANGLES, 0, 36);
-				}
+			glDrawArrays(GL_TRIANGLES, 0, 36); */
 
 			/* glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.) */
 			glfwSwapBuffers(window);
 			glfwPollEvents();
+
 		}
 
 	/* optional: de-allocate all resources once they've outlived their purpose: */
---
+++ Tutorial/tut_site/learn_opengl/Spotlight_realistic/basic_lighting.fs
@@ -0,0 +1,79 @@
+#version 460 core
+out vec4 FragColor;
+
+struct Material {
+	sampler2D diffuse;
+	sampler2D specular;    
+	float shininess;
+}; 
+
+struct Light {
+	vec3 position;
+	vec3 direction;
+  float cutOff;
+  float outerCutOff;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+
+	float constant;
+	float linear;
+	float quadratic;
+};
+
+in vec3 Normal;  
+in vec3 FragPos;  
+in vec2 TexCoords;
+
+/* the location of the light source */
+uniform vec3 viewPos;
+uniform Material material;
+uniform Light light;
+
+void main()
+{
+	/* ambient */
+	/* take the light’s color, multiply it with a small constant ambient factor */
+	vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+
+	/* diffuse */
+	/*  calculate is the direction vector between the light source and the fragment’s position */
+	/*  calculate this difference by subtracting both vectors. We also want to make sure all the relevant
+			vectors end up as unit vectors so we normalize both the normal and the resulting direction vector */
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(light.position - FragPos);
+	/* calculate the actual diffuse impact the light has on the current fragment by taking the
+			dot product of the norm and lightDir vector. The resulting value is then multiplied with the light’s color
+			to get the diffuse component, resulting in a darker diffuse component the greater the angle is between both
+			vectors */
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+	
+	/* Next we calculate the view direction vector and the corresponding reflect vector along the
+			normal axis:*/
+	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 reflectDir = reflect(-lightDir, norm); 
+	/* actually calculate the specular component. This is accomplished with the following formula */
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+	vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+
+	/* spotlight (soft edges) */
+	float theta = dot(lightDir, normalize(-light.direction)); 
+	float epsilon = (light.cutOff - light.outerCutOff);
+	float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+	diffuse  *= intensity;
+	specular *= intensity;
+
+	/* attenuation */
+	float distance    = length(light.position - FragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+
+	ambient  *= attenuation;  
+	diffuse   *= attenuation;
+	specular *= attenuation;   
+			
+	vec3 result = ambient + diffuse + specular;
+	FragColor = vec4(result, 1.0);
+		
+} 
